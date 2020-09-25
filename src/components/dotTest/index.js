6a9923dfec8190ed209dbfec21b0c5cdd88d2a65
Mehmet Celik <mehmet@celik.be>
2020-09-25 16:45:45 +0200

    disabled component

--- src/components/dotTest/index.js
+++ src/components/dotTest/index.js
@@ -11,7 +11,7 @@ export let component = {
         controller: DotTestController,
         template: "<div style='position:fixed;right: 0;top: 0;color:#009991;opacity:0.5;z-index: 100000;font-size: 10em;'>.</div>"
     },
-    enabled: true,
+    enabled: false,
     appendTo: 'prm-top-bar-before',
     enableInView: '.*'
 }
