faa74c6355e9d49b70069d233c744d0e136a0b3b
Benjamin Flämig <benjamin.flaemig@zhbluzern.ch>
2020-09-25 14:50:14 +0200

    Update index.js
    Addjusted images

--- src/components/searchAlsoBody/index.js
+++ src/components/searchAlsoBody/index.js
@@ -19,25 +19,27 @@ class SearchAlsoBodyController {
       {
         "name": "swisscovery RZS",
         "url": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/search?&tab=41SLSP_RZS_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_RZS:VU15&query=any,contains,",
-        "img": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU06/img/vector-combine.png",
+        "img": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU06/img/favicon_everything_view.png",
         "tooltip": "nui.customizing.idslu.search_also.tooltip.swisscovery-rzs",
         mapping: function mapping(search) {
           var terms = search.split(",");
           return terms[2] || "";
         }
-      }, /*{
-        "name": "swisscovery RZS - HSLU",
-        "url": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/search?&tab=41SLSP_RZS_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_RZS:VU06&query=any,contains,",
-        "img": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU07/img/vector-difference-ba.png",
-        "tooltip": "nui.customizing.idslu.search_also.tooltip.swisscovery-rzs-hslu",
-        mapping: function mapping(search) {
-          var terms = search.split(",");
-          return terms[2] || "";
-        }
-      },*/ {
+      },
+      /*{
+      "name": "swisscovery RZS - HSLU",
+      "url": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/search?&tab=41SLSP_RZS_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_RZS:VU06&query=any,contains,",
+      "img": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU07/img/vector-difference-ba.png",
+      "tooltip": "nui.customizing.idslu.search_also.tooltip.swisscovery-rzs-hslu",
+      mapping: function mapping(search) {
+       var terms = search.split(",");
+       return terms[2] || "";
+      }
+      },*/
+      {
         "name": "swisscovery RZS - ZHB/Uni/PH",
         "url": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/search?&tab=41SLSP_RZS_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_RZS:VU07&query=any,contains,",
-        "img": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU06/img/vector-difference-ab.png",
+        "img": "https://slsp-rzs.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU06/img/favicon_zhb_view.png",
         "tooltip": "nui.customizing.idslu.search_also.tooltip.swisscovery-rzs-zhbuniph",
         mapping: function mapping(search) {
           var terms = search.split(",");
@@ -46,7 +48,7 @@ class SearchAlsoBodyController {
       }, {
         "name": "swisscovery",
         "url": "https://slsp-network.primo.exlibrisgroup.com/discovery/search?&tab=41SLSP_NETWORK&search_scope=DN_and_CI&vid=41SLSP_NETWORK:VU1_UNION&query=any,contains,",
-        "img": "https://slsp-network.primo.exlibrisgroup.com/discovery/custom/41SLSP_NETWORK-VU1_UNION/img/library-logo.png",
+        "img": "https://slsp-network.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU06/img/favicon_swisscovery.png",
         "tooltip": "nui.customizing.idslu.search_also.tooltip.swisscovery",
         mapping: function mapping(search) {
           var terms = search.split(",");
@@ -77,7 +79,6 @@ class SearchAlsoBodyController {
           var type = type_mappings[terms[0]] || "kw";
           var query = terms[2] || "";
           return type + ':' + query;
-
         }
       }];
   }
