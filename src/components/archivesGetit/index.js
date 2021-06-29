d728d20582414e7b6b03cbd10b1151cc692b549f
Mehmet Celik <mehmet@celik.be>
2021-06-29 11:13:05 +0200

    archivesGetIt

---
+++ src/components/archivesGetit/index.js
@@ -0,0 +1,19 @@
+import 'primo-explore-eth-archives-getit';
+
+class ArchivesGetitController {
+  constructor() {
+    this.mainParentCtrl = this.parentCtrl;
+  }
+}
+
+export let rzsArchivesComponent = {
+    name: 'rzs-archives-getit',  
+    enabled: true,
+    appendTo: 'prm-alma-viewit-after',
+    enableInView: '.*',  
+    config: {      
+      bindings: {parentCtrl: '<'},
+      controller: ArchivesGetitController,
+      template: '<eth-archives-getit-component after-ctrl="$ctrl.mainParentCtrl"></eth-archives-getit-component>'
+    }
+  }
