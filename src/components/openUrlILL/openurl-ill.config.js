ecaa5787b32aefd596aaa959e6f62160e0a6dd70
Roth Florian <florian.roth@zhbluzern.ch>
2022-05-20 12:31:38 +0200

    update-html-and-logo

--- src/components/openUrlILL/openurl-ill.config.js
+++ src/components/openUrlILL/openurl-ill.config.js
@@ -2,18 +2,18 @@ export const ethOpenurlInterlibraryConfig = function(){
     return {
         label: {
             text:{
-                de: 'Artikel/Medium in Region Zentralschweiz RZS nicht vorhanden. <a href=" https://swisscovery.slsp.ch/discovery/search?vid=41SLSP_NETWORK:VU1_UNION">Suche schweizweit in swisscovery</a> oder kontaktieren Sie ',
-                en: 'Order a copy of the article via'
+                de: 'Artikel/Medium in Region Zentralschweiz RZS nicht vorhanden. <a href=" https://swisscovery.slsp.ch/discovery/search?vid=41SLSP_NETWORK:VU1_UNION" target="_blank">Suche schweizweit in swisscovery</a> oder kontaktieren Sie ',
+                en: 'Article/medium not available in region Central Switzerland RZS. <a href=" https://swisscovery.slsp.ch/discovery/search?vid=41SLSP_NETWORK:VU1_UNION&lang=en" target="_blank">Search Switzerland-wide</a> in swisscovery or contact '
             },
             linktext:{
-                de: 'Ihre Bibliothek',
-                en: 'Interlibrary loan'
+                de: 'Ihre Bibliothek.',
+                en: 'your library.'
             }
         },
         url:{
             link: {
                 de: 'https://www.rzsinfo.ch/bibliotheken',
-                en: 'https://mylibrary/ill/form'
+                en: 'https://www.rzsinfo.ch/bibliotheken'
             }
         }
     }
