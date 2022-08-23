3eed01d1e24aa3e0ec879a27f3e36be2c4162f5d
Roth Florian <florian.roth@zhbluzern.ch>
2022-08-23 13:41:12 +0200

    html-and-logos

---
+++ src/components/journalsStartPage/eth-journals-startpage/eth-journals-startpage.config_mod.js
@@ -0,0 +1,15 @@
+export const ethJournalsStartpageConfig = function(){
+    return {
+        label: {
+            heading: {
+                de: 'Zeitschriftensuche Region Zentralschweiz',
+                en: 'Journal search for holdings at ETH Zurich'
+            },
+            p1: {
+                de: '<p>Suche nach gedruckten und elektronischen Zeitschriften der Bibliotheken der Region Zentralschweiz.</p>  <p>Achten Sie darauf, dass Sie sich für lizenzierte E-Journals im entsprechenden Netz (vor Ort oder per VPN) befinden.</p> <p>Bei Fragen wenden Sie sich an Ihre <a href="https://www.rzsinfo.ch/bibliotheken"  target="_blank">Bibliothek</a>.</p> <p>Weiterer Sucheinstieg:</p><ul> <li><a href="https://browzine.com/libraries/1455/subjects?sort=title" target="_blank">Browzine UNI/PH</a> – blättern in ausgewählten E-Journals</li> 	<li><a href="https://browzine.com/libraries/1824/subjects?sort=title" target="_blank">Browzine HSLU</a> – blättern in ausgewählten E-Journals</li> </ul>  ',
+                en: 'Using the journal search, you can search the journal holdings of the ETH Zurich libraries – both the e-journals and printed journals.'
+            },
+
+        },
+    }
+}
