ff8102b1ab8e173f64fce8ba814c034165b0d061
unknown <semmi.ezaitouni@kuleuven.be>
2025-03-28 15:02:04 +0100

    removed unwanted title and cleanup

--- src/components/openingHours/seed/index.js
+++ src/components/openingHours/seed/index.js
@@ -1,52 +1,55 @@
 class openingHoursSeedController {
-    constructor($scope, $element, $compile, $timeout) {
-        let self = this;
-        let divs = $element.parent().parent();
+  constructor($scope, $element, $compile, $timeout) {
+    let self = this;
+    let divs = $element.parent().parent();
 
-        self.element = $element;
-        self.scope = $scope;
-        self.compile = $compile;
-        self.timeout = $timeout;
-        self.parentEl = divs[0];
+    self.element = $element;
+    self.scope = $scope;
+    self.compile = $compile;
+    self.timeout = $timeout;
+    self.parentEl = divs[0];
+  }
 
+  $onInit() {
+    let self = this;
+    let el = self.parentEl.querySelector("rzs-opening-hours");
+
+    if (!el) {
+      let target = self.element.parent().parent();
+
+      if (target) {
+        let elInfo = document.createElement("rzs-opening-hours");
+
+        let $elInfo = angular.element(elInfo);
+        let compiledElInfo = self.compile($elInfo);
+
+        //add new scope
+        compiledElInfo(self.scope.$parent.$new());
+
+        if (Array.isArray(Array.from(target))) {
+          target = target[0];
+        }
+        target.prepend(elInfo);
+      }
     }
-
-    $onInit() {
-        let self = this;
-        let el = self.parentEl.querySelector('rzs-opening-hours');
-
-        if (!el) {
-            let target = self.element.parent().parent()
- 
-            
-            if (target) {
-                let elInfo = document.createElement('rzs-opening-hours');
-                
-                let $elInfo = angular.element(elInfo);
-                let compiledElInfo = self.compile($elInfo);
-
-                //add new scope
-                compiledElInfo(self.scope.$parent.$new());
-                
-                if (Array.isArray(Array.from(target))) {
-                    target = target[0];
-                }
-                target.prepend(elInfo);            
-            }
-        }                
-    }
+  }
 }
 
-openingHoursSeedController.$inject = ['$scope', '$element', '$compile', '$timeout'];
+openingHoursSeedController.$inject = [
+  "$scope",
+  "$element",
+  "$compile",
+  "$timeout",
+];
 
 export let openingHoursSeedComponent = {
-    name: 'rsz-seed-opening-hours',
-    config: {
-        bindings: { parentCtrl: '<' },
-        controller: openingHoursSeedController,
-        template: ''
-    },
-    enabled: true,
-    appendTo: 'slsp-location-items-after',
-    enableInView: '.*'
-}
+  name: "rsz-seed-opening-hours",
+  config: {
+    bindings: { parentCtrl: "<" },
+    controller: openingHoursSeedController,
+    template: "",
+  },
+  enabled: true,
+  appendTo: "slsp-location-items-after",
+  enableInView: ".*",
+};
